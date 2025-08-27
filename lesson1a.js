8c3fc9c036b013e393e5e7bad200948931bfdf15
serakian <depril246@gmail.com>
2025-08-27 12:01:42 +0300

    javascript

---
+++ lesson1a.js
@@ -0,0 +1,6 @@
+// this is a comment
+console.log("Hello there. I'm learning Javascript")
+console.log("Hello world")
+console.log("Javascript is an interpreted language")
+console.log("It is also a high level language")
+console.log("It is used to create interactive UIs")
