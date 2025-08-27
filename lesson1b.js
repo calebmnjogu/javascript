3f901f2a8e9cf77a15edca2c73284b942eece4ca
serakian <depril246@gmail.com>
2025-08-27 12:34:51 +0300

    lesson1b.js

---
+++ lesson1b.js
@@ -0,0 +1,6 @@
+// Variables
+let weight = 10.5 //declaring a varaible with the initializer 'let'
+const pi = 3.142 //declaring a variable with the initializer 'const'
+//show output
+console.log(weight)
+console.log(pi)
