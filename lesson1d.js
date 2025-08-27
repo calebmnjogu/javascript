26f36d8c59c0135c47c1db76b0e292702b08ce1f
serakian <depril246@gmail.com>
2025-08-27 12:52:40 +0300

    lesson1d.js

---
+++ lesson1d.js
@@ -0,0 +1,29 @@
+//Data types
+// Integers-A whole number
+
+let age = 49
+console.log("Age of the person is: ",age)
+
+// Floating point ~ a number with a decimal
+ let quantity = 35.65
+
+ console.log("The quantity of the sugar bought is:",quantity)
+
+//  Boolean ~ A value that evaluates to either true or false
+
+let  isAdult = true;
+let isRegistered = false;
+
+console.log("The person an adult?",isAdult)
+console.log("Has he been registered? ",isRegistered)
+
+
+// Undefined ~ This is a data type that is declared without assigning any values
+
+let undefined;
+console.log(undefined)
+
+// null ~ It is a data type that contains null values
+
+let user = null;
+console.log(user)
