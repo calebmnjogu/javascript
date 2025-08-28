0f3193d035f9d3b8ff1fd6e2dd595f4a1f0096ec
serakian <depril246@gmail.com>
2025-08-28 12:28:38 +0300

    lesson2c.js

---
+++ lesson2c.js
@@ -0,0 +1,12 @@
+// IF conditional statement
+// It is used in the control flow execution structure of a program
+// It executes a block of cod /statement if and when a condition is met
+
+let age = 28;
+
+if(age < 18 ){
+    console.log("You are too young to watch this movie...")
+}
+else{
+    console.log("Sit down and enjoy the episode")
+}
