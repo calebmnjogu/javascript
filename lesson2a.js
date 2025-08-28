ed6b34a3df97f7b7f7c31dd54c998253ea3d1580
serakian <depril246@gmail.com>
2025-08-28 11:13:46 +0300

    lesson2a.js

---
+++ lesson2a.js
@@ -0,0 +1,26 @@
+// Javascript object data types
+// It is a data type that stores data in the form of key - value pair
+
+let person = {
+    Name : "Joseph William",
+    Age : 53,
+    isTeacher : true
+}
+
+// Below we print out the entire object
+console.log("The details of the person are: ",person)
+
+// How to access specific values of an object
+// We use the dot separator to do this
+
+console.log("The name of the person is: ",person.Name)
+console.log("The age of the person is: ",person.Age)
+console.log("Is the person a teacher?: ",person.isTeacher)
+
+
+// Array data type
+// This refers a collection of items that are on indexes
+fruits = ["Apple","Mango","Banana","Lemon","Grapes","Tomato","Oranges","Berries"]
+
+console.log("The list of fruits is:",fruits)
+console.log("The sour fruit is:",fruits[3])
