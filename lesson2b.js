84502813059df95b58637262364aeb4892706ca4
serakian <depril246@gmail.com>
2025-08-28 12:02:19 +0300

    lesson2b.js

---
+++ lesson2b.js
@@ -0,0 +1,31 @@
+// Comparison operators
+// Used to evaluate/equate different values.
+// They give a boolean answer
+
+let number1 = 10
+let number2 = 20
+
+console.log(number1>number2)
+console.log(number1<number2)
+console.log(number1>=number2)
+console.log(number1<=number2)
+console.log(number1==number2)
+console.log(number1!=number2)
+console.log(number1===number2) //"===" not only compares the values but also the data types
+
+
+// Logical operators
+// Logical AND (&&)
+// It evaluates to true if and only both statements are true
+
+console.log((number1<number2)&&(number2>number1))
+
+// logical OR(||)
+// It evaluates to true if one of the statements is true
+console.log((number1<=number2)||(number2>number1))
+
+// Logical NOT(!)
+// It negates(gives the opposite of something)
+
+console.log(!(number1>number2))
+
