4d0d8e318a360569e7a341eb0131c6383f9e9159
serakian <depril246@gmail.com>
2025-08-28 12:41:15 +0300

    lesson2d.js

---
+++ lesson2d.js
@@ -0,0 +1,18 @@
+// If...else if ....else statement
+// It is used to evaluate multiple conditions
+
+
+let age = 14
+
+if (age < 15){
+    console.log("You are too young to watch this episode,")
+}
+else if(age>=15 && age <=18){
+    console.log("You can watch this movie with parental guidance")
+}
+else if(age>=18 && age<40){
+    console.log("Have a seat and enjoy the movie")
+}
+else{
+    console.log("This movie does not suit you")
+}
