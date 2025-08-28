64b024dc5563e4a30cd4d20faf7d374831be9dc1
serakian <depril246@gmail.com>
2025-08-28 11:32:23 +0300

    lesson2a.js

---
+++ task1.js
@@ -0,0 +1,13 @@
+let car = {
+    Name : "Toyota Vanguard",
+    model :  2014,
+    Colour : "white",
+    cc : 2200
+}
+console.log(car)
+console.log("The name of the car is:",car.Name)
+
+let sports = ["Football","Formula One","soccer","Rugby","Tennis"]
+
+console.log("We have various sports e.g",sports)
+console.log("The most famous sport is:",sports[2])
