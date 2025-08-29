d7a07a1f00e4acf345f96cc3860babe8578ad5c3
serakian <depril246@gmail.com>
2025-08-29 11:51:17 +0300

    lesson3a.js

---
+++ lesson3a.js
@@ -0,0 +1,25 @@
+//Java script For loop
+// Loops are used to iterate through something
+// Structure of the for loop
+//  1. Start value
+//  2.condition
+//  3.Increment/decrement
+//  4.Body ~ Statement to be printed out
+
+// for (let i = 0; i <= 10; i++){
+//     console.log("The new value of i is:", i)
+// }
+
+// for (let year = 2000 ; year<=2020 ; year+=4){
+//     console.log("The leap years between 1999 and 2021 are: ",year)
+// }
+
+for (let num =10 ; num>=1 ; num--){
+    if (num == 5){
+        console.log("here is number five")
+    }
+    else{
+        console.log(num)
+}
+    }
+
