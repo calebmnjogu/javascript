1d85920e0ddd66cb3d535355998a34a91c8fd4ba
serakian <depril246@gmail.com>
2025-08-29 12:00:25 +0300

    lesson3b.js

---
+++ lesson3b.js
@@ -0,0 +1,20 @@
+// While loop
+// It is commonly used when the length of items is not known
+// Structure: 1. Initialization of a variable
+// 2. While keyword
+// 3. condition to be checked
+// 4.Increment/decrement
+
+// let i = 0
+
+// while( i <= 10){
+//     console.log(i)
+//     i++
+// }
+
+let num=10
+
+while(num >=1){
+    console.log(num)
+    num--
+}
